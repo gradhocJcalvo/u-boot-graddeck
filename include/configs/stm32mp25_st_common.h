413783d9427eb438ebe4208f545a9fbc74018ba2
Patrice Chotard <patrice.chotard@foss.st.com>
2024-08-27 20:53:46 +0200

    configs: stm32mp25: update boot_targets when boot_device is set to nand or spi-nand
    When boot_device is nand or spi-nand, bootfs can be located in ubifs or
    mmc0.
    Update boot_targets to "ubifs mmc0" to search bootfs first on ubifs,
    then mmc0.
    
    Signed-off-by: Patrice Chotard <patrice.chotard@foss.st.com>
    Change-Id: I6a299a9535886d55a810c2d99cc436a3d2fe14b7
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/400566
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Tested-by: Patrick DELAUNAY <patrick.delaunay@foss.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Reviewed-by: Patrick DELAUNAY <patrick.delaunay@foss.st.com>
    Domain-Review: Patrick DELAUNAY <patrick.delaunay@foss.st.com>

--- include/configs/stm32mp25_st_common.h
+++ include/configs/stm32mp25_st_common.h
@@ -19,7 +19,8 @@
  * default bootcmd for stm32mp25 STMicroelectronics boards:
  * for serial/usb: execute the stm32prog command
  * for mmc boot (eMMC, SD card), distro boot on the same mmc device
- * for nand or spi-nand boot, distro boot with ubifs on UBI partition
+ * for nand or spi-nand boot, distro boot with ubifs on UBI partition or
+ * sdcard
  * for nor boot, distro boot on SD card = mmc0 ONLY !
  */
 #define ST_STM32MP25_BOOTCMD "bootcmd_stm32mp=" \
@@ -32,7 +33,7 @@
 		"then env set boot_targets \"mmc${boot_instance}\"; fi;" \
 		"if test ${boot_device} = nand ||" \
 		  " test ${boot_device} = spi-nand ;" \
-		"then env set boot_targets ubifs0; fi;" \
+		"then env set boot_targets ubifs0 mmc0; fi;" \
 		"if test ${boot_device} = nor;" \
 		"then env set boot_targets mmc0; fi;" \
 		"run distro_bootcmd;" \
