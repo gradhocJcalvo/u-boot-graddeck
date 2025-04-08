STMicroelectronics/u-boot

arm: stm32mp: add support of STM32MP21x
Introduce the code in mach-stm32mp, the device tree and the configuration
files for the new STM32MP family.

Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>
Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>
Signed-off-by: Patrice Chotard <patrice.chotard@foss.st.com>
Change-Id: I90b7a731f3e836dd14d33020218af1a7e1cc5f9f
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/360527
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/360531

arch/arm/mach-stm32mp/stm32mp2/arm64-mmu.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later OR BSD-3-Clause
 /*
- * Copyright (C) 2023, STMicroelectronics - All Rights Reserved
+ * Copyright (C) 2023-2024, STMicroelectronics - All Rights Reserved
  */
 
 #include <common.h>
@@ -16,6 +16,17 @@
 
 struct mm_region stm32mp2_mem_map[MP2_MEM_MAP_MAX] = {
 	{
+#if defined(CONFIG_STM32MP21X)
+		/* RETRAM, SRAM1, SYSRAM: alias1 */
+		.virt = 0x20000000UL,
+		.phys = 0x20000000UL,
+		.size = 0x00070000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
+			 PTE_BLOCK_NON_SHARE |
+			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
+	}, {
+#endif
+#if defined(CONFIG_STM32MP25X)
 		/* PCIe */
 		.virt = 0x10000000UL,
 		.phys = 0x10000000UL,
@@ -32,6 +43,7 @@ struct mm_region stm32mp2_mem_map[MP2_MEM_MAP_MAX] = {
 			 PTE_BLOCK_NON_SHARE |
 			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
 	}, {
+#endif
 		/* Peripherals: alias1 */
 		.virt = 0x40000000UL,
 		.phys = 0x40000000UL,

gradhocJcalvo/u-boot-graddeck

arch/arm/mach-stm32mp/stm32mp2/arm64-mmu.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later OR BSD-3-Clause
 /*
- * Copyright (C) 2023, STMicroelectronics - All Rights Reserved
+ * Copyright (C) 2023-2024, STMicroelectronics - All Rights Reserved
  */
 
 #include <common.h>
@@ -16,6 +16,17 @@
 
 struct mm_region stm32mp2_mem_map[MP2_MEM_MAP_MAX] = {
 	{
+#if defined(CONFIG_STM32MP21X)
+		/* RETRAM, SRAM1, SYSRAM: alias1 */
+		.virt = 0x20000000UL,
+		.phys = 0x20000000UL,
+		.size = 0x00200000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
+			 PTE_BLOCK_NON_SHARE |
+			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
+	}, {
+#endif
+#if defined(CONFIG_STM32MP25X)
 		/* PCIe */
 		.virt = 0x10000000UL,
 		.phys = 0x10000000UL,
@@ -32,6 +43,7 @@ struct mm_region stm32mp2_mem_map[MP2_MEM_MAP_MAX] = {
 			 PTE_BLOCK_NON_SHARE |
 			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
 	}, {
+#endif
 		/* Peripherals: alias1 */
 		.virt = 0x40000000UL,
 		.phys = 0x40000000UL,