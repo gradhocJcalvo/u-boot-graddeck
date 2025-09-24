d7e73d7f2320af88e550cc5eef59ae58dfcc5aa6
gradhoc-jcalvo <jcalvo@gradhoc.com>
2025-09-24 12:00:54 +0200

    Custom U-Boot GradDeck

---
+++ include/config.h
@@ -0,0 +1,6 @@
+/* Automatically generated - do not edit */
+#define CFG_BOARDDIR board/st/stm32mp2
+#include <configs/stm32mp25_st_common.h>
+#include <asm/config.h>
+#include <linux/kconfig.h>
+#include <config_fallbacks.h>
